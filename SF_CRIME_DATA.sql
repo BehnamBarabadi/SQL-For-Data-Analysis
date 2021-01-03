72b2be3d56e7e0004e12f8d96260d56810ef250e
Behnam Barabadi <BehnamBarabadi@gmail.com>
2021-01-02 20:14:50 -0800

    data added

---
+++ SF_CRIME_DATA.sql
@@ -0,0 +1,18 @@
+CREATE TABLE sf_crime_data(
+id INT,
+day_of_week VARCHAR(60),
+date VARCHAR(60),
+time VARCHAR(60),sf_crime_data
+primary key (id));
+
+INSERT INTO sf_crime_data VALUES (1,'friday','01/31/2014 08:00:00 AM +0000','7:37');
+INSERT INTO sf_crime_data VALUES (2,'friday','01/26/2014 08:00:00 AM +0000','8:17');
+INSERT INTO sf_crime_data VALUES (3,'saturday','01/23/2014 08:00:00 AM +0000','12:30');
+INSERT INTO sf_crime_data VALUES (4,'saturday','01/21/2014 08:00:00 AM +0000','17:7');
+INSERT INTO sf_crime_data VALUES (5,'sunday','01/22/2014 08:00:00 AM +0000','21:22');
+INSERT INTO sf_crime_data VALUES (6,'monday','01/15/2014 08:00:00 AM +0000','18:37');
+INSERT INTO sf_crime_data VALUES (7,'monday','02/30/2014 08:00:00 AM +0000','7:56');
+INSERT INTO sf_crime_data VALUES (8,'tuesday','04/1/2014 08:00:00 AM +0000','4:27');
+INSERT INTO sf_crime_data VALUES (9,'thursday','06/2/2014 08:00:00 AM +0000','9:57');
+INSERT INTO sf_crime_data VALUES (10,'monday','08/25/2014 08:00:00 AM +0000','20:27');
+INSERT INTO sf_crime_data VALUES (11,'friday','11/16/2014 08:00:00 AM +0000','14:55');
